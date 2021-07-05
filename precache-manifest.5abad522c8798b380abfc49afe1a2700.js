8a54d1368b3e908cbdc245b806cf6b21e3a37b43
Nipu Chakraborty <nipu.chakraborty@bongobd.com>
2021-07-05 11:15:27 +0600

    Updates

---
+++ precache-manifest.5abad522c8798b380abfc49afe1a2700.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a9c48486f0f4ae87c16169e316c40b",
+    "url": "/bongobd-task/problem-3-video-player/index.html"
+  },
+  {
+    "revision": "6f310de8bf6a265f2fb4",
+    "url": "/bongobd-task/problem-3-video-player/static/css/main.5514d8a5.chunk.css"
+  },
+  {
+    "revision": "9089f70a8bb3cde332f8",
+    "url": "/bongobd-task/problem-3-video-player/static/js/2.738ff21e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bongobd-task/problem-3-video-player/static/js/2.738ff21e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f310de8bf6a265f2fb4",
+    "url": "/bongobd-task/problem-3-video-player/static/js/main.4388b7ce.chunk.js"
+  },
+  {
+    "revision": "e3f1828a2a99d08bd65a",
+    "url": "/bongobd-task/problem-3-video-player/static/js/runtime-main.4d86e060.js"
+  },
+  {
+    "revision": "959214216954e9d0cdf00c8a62a49c2f",
+    "url": "/bongobd-task/problem-3-video-player/static/media/corona-virus.95921421.mp4"
+  },
+  {
+    "revision": "3eeec97f8eb5947b4c40df613489b7b3",
+    "url": "/bongobd-task/problem-3-video-player/static/media/top-5-react-framework.3eeec97f.mp4"
+  }
+]);
